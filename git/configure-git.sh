57771f2d47be57608570fbf3d4352a104b330929
Joe Butler <joebutler88@gmail.com>
2019-12-02 11:56:41 -0800

    Git additions
    Adds several aliases, configures user.email if GIT_EMAIL is set, and
    adds a global gitignore for filtering OS generated files that we'll
    universally never want.

--- git/configure-git.sh
+++ git/configure-git.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# This is a baseline gitconfig. Things like email
-# should be added in separate machine specific files
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
 
+# Baseline gitconfig
 gcg='git config --global'
 
 $gcg user.name 'Joe Butler'
+if [ "$GIT_EMAIL" = "" ];
+then
+    echo -e "${YELLOW}[WARN] GIT_EMAIL variable is not defined. Will not set git user.email${NC}"
+else
+    $gcg user.email $GIT_EMAIL
+fi
 
 # Default git pagination settings are less than ideal
 # for things like `git branch` if the size of the output
@@ -15,3 +22,11 @@ $gcg core.pager "less -FRX"
 
 # Store github access token on the filesystem
 $gcg credential.helper store
+
+# Global git ignore
+$gcg core.excludesfile ~/.gitignore_global
+
+# Aliases
+$gcg alias.amend "commit --amend --no-edit"
+$gcg alias.alias "! git config --get-regexp ^alias\. | sed -e s/^alias\.// -e s/\ /\ =\ /"
+$gcg alias.nah   "!git reset --hard && git clean -df"
