9ab0a44bf4e0c6456501009238dbbf34cc7036c1
Joe Butler <joebutler88@gmail.com>
2024-04-06 22:11:40 -0600

    fix: port configure-git script to zsh

--- git/configure-git.sh
+++ git/configure-git.sh
@@ -1,41 +1,42 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-# Baseline gitconfig
-gcg='git config --global'
+gcg() {
+    git config --global $1 $2
+}
 
-$gcg user.name 'Joe Butler'
+gcg user.name 'Joe Butler'
 if [ "$GIT_EMAIL" = "" ];
 then
     echo -e "${YELLOW}[WARN] GIT_EMAIL variable is not defined. Will not set git user.email${NC}"
 else
-    $gcg user.email $GIT_EMAIL
+    gcg user.email $GIT_EMAIL
 fi
 
 # Default git pagination settings are less than ideal
 # for things like `git branch` if the size of the output
 # is small. Make `less` behave similarly to `cat` if
 # the output can fit on one screen, else paginate normally
-$gcg core.pager "less -FRX"
+gcg core.pager "less -FRX"
 
 # Store github access token on the filesystem
-$gcg credential.helper store
+gcg credential.helper store
 
 # Global git ignore
-$gcg core.excludesfile ~/.gitignore
+gcg core.excludesfile ~/.gitignore
 
 # Solve racial inequality
-$gcg init.defaultBranch main
+gcg init.defaultBranch main
 
 # Misc
-$gcg push.autoSetupRemote true
+gcg push.autoSetupRemote true
 
 # Aliases
-$gcg alias.amend "commit --amend --no-edit"
-$gcg alias.alias "! git config --get-regexp ^alias\. | sed -e s/^alias\.// -e s/\ /\ =\ /"
-$gcg alias.nah   "!git reset --hard && git clean -df"
+gcg alias.amend "commit --amend --no-edit"
+gcg alias.alias "! git config --get-regexp ^alias\. | sed -e s/^alias\.// -e s/\ /\ =\ /"
+gcg alias.nah   "!git reset --hard && git clean -df"
 
 # Merge gitignore template files into single global gitignore
 cat $(dirname "$0")/gitignore/* > ~/.config/git/ignore
