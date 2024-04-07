f2998031dec31f19076621c6a6beca88012622be
Joe Butler <joebutler88@gmail.com>
2024-04-06 21:36:39 -0600

    move more shit out of HOME

---
+++ zsh/custom/50-aws.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+export AWS_SHARED_CREDENTIALS_FILE="$XDG_CONFIG_HOME/aws/credentials"
+export AWS_CONFIG_FILE="$XDG_CONFIG_HOME/aws/config"
--- zsh/custom/50-rust.zsh
+++ zsh/custom/50-rust.zsh
@@ -1,3 +1,6 @@
 #!/usr/bin/env zsh
 
-[[ -f "$HOME/.cargo/env" ]] && source "$HOME/.cargo/env"
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+
+[[ -f "$CARGO_HOME/env" ]] && source "$CARGO_HOME/env"
