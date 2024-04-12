8ffdb56f135e7a3d255f081e0f40f8b431e79e05
Joe Butler <joebutler88@gmail.com>
2024-04-11 21:12:59 -0600

    fix: bug where EDITOR is cleared if nvim isn't installed

--- zsh/custom/97-editor.zsh
+++ zsh/custom/97-editor.zsh
@@ -8,10 +8,10 @@ if which vim >/dev/null 2>&1; then
     vim_path="$(which vim 2>/dev/null)"
 fi
 
-if [[ -n "nvim_path" ]]; then
+if [ -n "$nvim_path" ]; then
     alias vim="$nvim_path"
     export EDITOR="$nvim_path"
-elif [[ -n "vim_path" ]]; then
+elif [ -n "$vim_path" ]; then
     export EDITOR="$vim_path"
 fi
 
