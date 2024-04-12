fb1db265d07f3471090f90b8b4c44c684351b314
Joe Butler <joebutler88@gmail.com>
2024-04-11 20:57:07 -0600

    feat: generalize pathmunge to be used elsewhere
    For example, you can now use munge to update LIBRARY_PATH the same way
    you'd normally update PATH. Also fixes a bug that resulted in an extra
    colon being prepended if the env var is initially empty.

--- zsh/custom/01-core.zsh
+++ zsh/custom/01-core.zsh
@@ -1,19 +1,27 @@
 #!/usr/bin/env zsh
 
-function pathmunge() {
-    if ! echo $PATH | grep -E -q "(^|:)$1($|:)"; then
-        if [ "$2" = "after" ] ; then
-            PATH=$PATH:$1
+munge() {
+    if [ -z "$1" ]; then
+        echo "$2"
+    elif ! echo "$1" | grep -E -q "(^|:)$2($|:)"; then
+        if [ "$3" = "after" ] ; then
+            echo "$1:$2"
         else
-            PATH=$1:$PATH
+            echo "$2:$1"
         fi
+    else
+        echo "$1"
     fi
 }
 
-function pathmunge_reorder() {
+pathmunge() {
+    PATH=$(munge "$PATH" "$1" "$2")
+}
+
+pathmunge_reorder() {
     PATH="$(echo "$PATH" | sed -E -e "s#(^|:)$1($|:)#:#" | sed -E -e 's/(^:|:$)//')"
     pathmunge "$@"
 }
 
 # Configure a basic path if there is not one. Should be fine for most use cases.
-[[ -z "$PATH" ]] && PATH="/usr/bin:/usr/sbin:/bin:/sbin"
+[ -z "$PATH" ] && PATH="/usr/bin:/usr/sbin:/bin:/sbin"
--- zsh/custom/os/osx.zsh
+++ zsh/custom/os/osx.zsh
@@ -2,7 +2,7 @@
 
 # brew
 pathmunge "/opt/homebrew/bin"
-export LIBRARY_PATH="$LIBRARY_PATH:$(brew --prefix)/lib"
+LIBRARY_PATH="$(munge "$LIBRARY_PATH" "$(brew --prefix)/lib")"
 
 # java
-[[ -e "/opt/homebrew/opt/openjdk/bin" ]] && pathmunge "/opt/homebrew/opt/openjdk/bin"
+[ -e "/opt/homebrew/opt/openjdk/bin" ] && pathmunge "/opt/homebrew/opt/openjdk/bin"
